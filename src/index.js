601b720c6d186685bb64c4b2cbaa5b3b33e70704
Chris Cresswell <christorper@gmail.com>
2021-12-02 23:39:02 -0800

    Load html with pageLoad module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello');
+import pageLoad from './page-load.js';
+
+pageLoad();
--- src/page-load.js
+++ src/page-load.js
@@ -1,8 +1,48 @@
 const pageLoad = () => {
-    let mainContainer = document.getElementById('content');
-    let newDiv = document.createElement('div');
-    newDiv.textContent = 'aaaa';
-    mainContainer.appendChild(newDiv);
+
+    const contentContainer = document.getElementById('content');
+
+    const makeSidebar = (() => {
+    
+    let sideBar = document.createElement('div');
+    sideBar.classList.add('sidebar');
+    let nav = document.createElement('ul');
+
+    let listItems = ['Menu', 'Reviews', 'About', 'Contact'];
+
+    for(let i = 0; i < 4; ++i)
+    {
+        let listElement = document.createElement('li');
+        let navLink = document.createElement('a');
+        navLink.textContent = listItems[i];
+        listElement.appendChild(navLink);
+        nav.appendChild(listElement);
+    }
+    sideBar.appendChild(nav);
+    contentContainer.appendChild(sideBar);
+})();
+
+const makeMain = (() => {
+    let main = document.createElement('div');
+    let header = document.createElement('div');
+    header.classList.add('header');
+    header.textContent = 'An Awesome Restaurant';
+    main.classList.add('main-container');
+    let blurbAndCat = document.createElement('div');
+    blurbAndCat.classList.add('blurb-and-cat');
+    let blurb = document.createElement('div');
+    blurb.classList.add('blurb');
+    blurb.textContent = 'This restaurant is really great. I can\'t believe I ate here! I have two cats.'
+    let cat = document.createElement('div');
+    cat.classList.add('cat');
+    blurbAndCat.appendChild(blurb);
+    blurbAndCat.appendChild(cat);
+    main.appendChild(header);
+    main.appendChild(blurbAndCat);
+    contentContainer.appendChild(main);
+})();
+
+return { makeSidebar, makeMain };
 }
 
 export default pageLoad();  
